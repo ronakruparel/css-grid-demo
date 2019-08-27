73cac06daf53fc9e319d194e42326992277916f0
ronakruparel <ronak.ruparel@accionlabs.com>
2019-08-27 19:52:42 +0530

    feat(app):added back to top button

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+/**
+ * handles the toggling of color and display
+ * of read more and read less buttons
+ */
+
 function toggleReadmore() {
   let btnText = document.getElementById("readMore");
   let dots = document.getElementById("dots");
@@ -12,4 +17,26 @@ function toggleReadmore() {
     btnText.innerHTML = "Read Less";
     btnText.style.color = "#609";
   }
+}
+
+/**
+ * display back to top button on scroll value of above 300
+ */
+
+window.onscroll = function handleScroll() {
+  let button = document.getElementById("backToTop");
+  let scrollPosition = document.body.scrollTop;
+  if (scrollPosition > 300) {
+    button.style.display = "inline";
+  } else {
+    button.style.display = "none";
+  }
+};
+
+/**
+ * reset the scroll position to 0
+ */
+function handleClick() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
